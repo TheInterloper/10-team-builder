c5c97a828fca7601cf5c807a5e2e175a370e980b
Tyler Olson <t19.olson@gmail.com>
2022-11-07 19:03:47 -0600

    enigineer test written

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,1 +1,17 @@
-const engineer = require('../lib/engineer')
+const Engineer = require('../lib/engineer')
+
+describe('getTitle()', () => {
+  it('Get title', () => {
+    const title = 'Engineer';
+    const newEmployee = new Employee('Bob', 4, 'bob@example.com', title)
+    expect(newEmployee.getTitle()).toBe(title);
+  })
+})
+
+describe('get Github()', () => {
+  it('Get Github Username', () => {
+    const github = 'githubUsername'
+    const newEmployee = new Engineer('Bob', 4, 'bob@example.com', github)
+    expect(newEmployee.github()).toBe(github)
+  })
+})
