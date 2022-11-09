845d1c28739e768201538211e8d7c1d6d9571f04
Tyler Olson <t19.olson@gmail.com>
2022-11-08 18:57:07 -0600

    HTML writing okayish

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const userData = require('./public/htmlinfo');
+const userData = require('./public/htmlinfo')
 const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-const team = []
-
+// inquirer answers pushed to this array
+let team = []
 
 //Write HTML file
 
-function writeToFile(data) {
-  fs.writeFile('./dist/index.html', team, (err)=>
-    err ? console.log("error") : console.log('HTML created!'))
+function writeToFile(team) {
+  const responses = userData(team)
+  // const responses = createTeamBadge(team)
+  fs.writeFile('./dist/index.html', responses, (err) =>
+    err ? console.log("error") : console.log('HTML page created!'))
 }
 
 //Terminal prompts for cards
@@ -25,114 +27,100 @@ function writeToFile(data) {
 
 
 
-async function empAdd(){
+async function empAdd() {
   return inquirer
     .prompt([
       {
         type: 'list',
-
         message: 'What team member type are you adding?',
-        
         name: 'role',
-        
         choices: ['Manager', 'Engineer', 'Intern'],
       },
       {
         type: 'input',
-        
         message: 'What is the team members name?',
-        
         name: 'name',
       },
       {
         type: 'input',
-        
         message: 'What is the team members ID number?',
-        
         name: 'id',
       },
       {
         type: 'input',
-        
         message: 'What is the team members email?',
-        
         name: 'email',
       }
     ])
 }
 
 //Funcs for case based responses
-function managerRole(){
+function managerRole() {
   return inquirer
-  .prompt([
-    {
-      type: 'input',
-      
-      message: 'What is the managers office number?',
-      
-      name: 'officeNum',
-    }
-  ])
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the managers office number?',
+        name: 'officeNum',
+      }
+    ])
 }
 
-function engineerRole(){
+function engineerRole() {
   return inquirer
-  .prompt([
-    {
-      type: 'input',
-      
-      message: 'What is the engineers GitHub URL?',
-      
-      name: 'github',
-    }
-  ])
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the engineers GitHub URL?',
+        name: 'github',
+      }
+    ])
 }
 
-function internRole(){
+function internRole() {
   return inquirer
-  .prompt([
-    {
-      type: 'input',
-      
-      message: 'What is the interns school name?',
-      
-      name: 'school',
-    }
-  ])
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the interns school name?',
+        name: 'school',
+      }
+    ])
 }
 
-function additonalEmployee(){
+function additonalEmployee() {
   return inquirer
-  .prompt([
-    {
-      type: 'confirm',
-      
-      message: 'Would you like to add another employee?',
-      
-      name: 'add',
-    }
-  ])
+    .prompt([
+      {
+        type: 'confirm',
+        message: 'Would you like to add another employee?',
+        name: 'add',
+      }
+    ])
 }
 
-async function init(){
+async function init() {
   let addMore = true
 
   while (addMore) {
     let data = await empAdd();
 
-    if (data.role === 'Manager'){
-      await managerRole()
-    
-    } else if (data.role === 'Engineer'){
-      await engineerRole()
+    if (data.role === 'Manager') {
+      const { officeNum } = await managerRole()
+      team.push(new Manager(data.name, data.id, data.email, officeNum))
+
+    } else if (data.role === 'Engineer') {
+      const { github } = await engineerRole()
+      team.push(new Engineer(data.name, data.id, data.email, github))
 
     } else {
-      await internRole()
-    } 
+      const { school } = await internRole()
+      team.push(new Intern(data.name, data.id, data.email, school))
+    }
 
     let answer = await additonalEmployee()
 
-    if (!answer.add){
+    if (!answer.add) {
       addMore = false
     }
   }
@@ -140,6 +128,4 @@ async function init(){
   writeToFile(team)
 
 }
-
-
 init()
--- public/htmlinfo.js
+++ public/htmlinfo.js
@@ -1,5 +1,53 @@
-function htmlInfo(data){
-  return ` `
-}
+function htmlInfo(team) {
+  let badges = ""
 
-  module.exports = htmlInfo;
+  for (let i = 0; i < team.length; i++) {
+    const title = team[i].getEmployeeTitle()
+    if (title === 'Manager') {
+      `<li> Office: ${team[i].officeNum}</li>`
+    } else if (title === 'Engineer') {
+      `<li> Github: ${team[i].github}</li>`
+    } else if (title === 'Intern') {
+      `<li> School: ${team[i].school}</li>`
+    }
+
+    const badge =
+      `
+        <div>
+          <h4>${team[i].name}</h4>
+          <p>${team[i].getEmployeeTitle()}</p>
+        </div>
+        <ul>
+          <li> ID: ${team[i].id}</li>
+          <li> Email: <a href="mailto:${team[i].email}"> ${team[i].email}</a></li>
+        </ul>
+      `
+    badges += badge
+  }
+
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+      <header>
+        <h1>Employee Team</h1>
+      </header>
+    
+      <div>
+        ${badges}              
+      </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+    </html>`
+}
+module.exports = htmlInfo;
